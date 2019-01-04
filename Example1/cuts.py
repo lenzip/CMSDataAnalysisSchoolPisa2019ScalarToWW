5ca82b665e812a8c644e47e83b26ab8a27e5228a
Piergiulio <piergiulio.lenzi@cern.ch>
2019-01-04 16:07:38 +0100

    first commit

---
+++ Example1/cuts.py
@@ -0,0 +1,29 @@
+# cuts
+
+#supercut is applied to all other cuts  
+supercut = 'mll>12  \
+            && std_vector_lepton_pt[0]>25 && std_vector_lepton_pt[1]>10 \
+            && std_vector_lepton_pt[2]<10 \
+            && metPfType1 > 20 \
+            && ptll > 30 \
+            && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13) \
+           '
+
+# cuts are defined with a name and the selection
+
+# DT->tautau control region with 0 jets
+cuts['hww2l2v_13TeV_dytt_of0j']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13)    \
+                && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
+                && ( mth<60) \
+                && mll>40 && mll<80 \
+                && ( std_vector_jet_pt[0] < 30 ) \
+                && '+bVeto+' \
+                '
+# top control region with 1 jet                
+cuts['hww2l2v_13TeV_top_of1j']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13)    \
+                && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
+                && mll>50 \
+                && ( std_vector_jet_pt[0] >= 30 ) \
+                && ( std_vector_jet_pt[1] < 30 ) \
+                && std_vector_jet_cmvav2[0]>-0.5884 \
+                '
---
+++ Example1/variables.py
@@ -0,0 +1,114 @@
+# variables
+
+#variables = {}
+    
+#'fold' : # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
+   
+variables['events']  = {   'name': '1',
+                        'range' : (1,0,2),
+                        'xaxis' : 'events',
+                         'fold' : 3
+                        }
+
+variables['drll']  = {  'name': 'drll',        #   variable name    
+                        'range' : (100,0,2),    #   variable range
+                        'xaxis' : 'DR_{ll}',    #   x axis name
+                        'fold' : 3
+                        }
+
+
+variables['nvtx']  = {   'name': 'nvtx',      
+                        'range' : (40,0,40),  
+                        'xaxis' : 'nvtx', 
+                        'fold' : 3
+                        }
+
+variables['mll']  = {   'name': 'mll',            #   variable name    
+                        'range' : (20,10,200),    #   variable range
+                        'xaxis' : 'm_{ll} [GeV]',  #   x axis name
+                         'fold' : 0
+                        }
+                        
+variables['mth']  = {   'name': 'mth',            #   variable name    
+                        'range' : (10,60,200),    #   variable range
+                        'xaxis' : 'm_{T}^{H} [GeV]',  #   x axis name
+                        'fold' : 0
+                        }
+
+variables['ptll']  = {   'name': 'ptll',            #   variable name    
+                        'range' : (20,0,200),    #   variable range
+                        'xaxis' : 'pt_{ll} [GeV]',  #   x axis name
+                        'fold' : 0
+                        }
+
+variables['met']  = {   'name': 'metPfType1',            #   variable name    
+                        'range' : (20,0,200),    #   variable range
+                        'xaxis' : 'pfmet [GeV]',  #   x axis name
+                        'fold' : 0
+                        }
+
+variables['dphill']  = {   'name': 'abs(dphill)',     
+                        'range' : (20,0,3.14),   
+                        'xaxis' : '#Delta#phi_{ll}',
+                        'fold' : 3
+                        }
+
+variables['pt1']  = {   'name': 'std_vector_lepton_pt[0]',     
+                        'range' : (40,0,200),   
+                        'xaxis' : 'p_{T} 1st lep',
+                        'fold'  : 0                         
+                        }
+
+variables['pt2']  = {   'name': 'std_vector_lepton_pt[1]',     
+                        'range' : (40,0,100),   
+                        'xaxis' : 'p_{T} 2nd lep',
+                        'fold'  : 0                         
+                        }
+
+
+
+variables['eta1']  = {  'name': 'std_vector_lepton_eta[0]',     
+                        'range' : (20,-3,3),   
+                        'xaxis' : '#eta 1st lep',
+                        'fold'  : 3                         
+                        }
+
+variables['eta2']  = {  'name': 'std_vector_lepton_eta[1]',     
+                        'range' : (20,-3,3),   
+                        'xaxis' : '#eta 2nd lep',
+                        'fold'  : 3                         
+                        }
+
+variables['njet']  = {
+                        'name': 'njet',     
+                        'range' : (5,0,5),   
+                        'xaxis' : 'Number of jets',
+                        'fold' : 2   # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
+                        }
+
+variables['jetpt1']  = {
+                        'name': 'std_vector_jet_pt[0]',     
+                        'range' : (40,0,200),   
+                        'xaxis' : 'p_{T} 1st jet',
+                        'fold' : 2   # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
+                        }
+
+variables['jetpt2']  = {
+                        'name': 'std_vector_jet_pt[1]',     
+                        'range' : (40,0,200),   
+                        'xaxis' : 'p_{T} 2nd jet',
+                        'fold' : 2   # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
+                        }
+
+variables['jeteta1']  = {  'name': 'std_vector_jet_eta[0]',
+                        'range' : (80,-5.0,5.0),
+                        'xaxis' : '#eta 1st jet',
+                        'fold'  : 0
+                        }
+
+variables['jeteta2']  = {  'name': 'std_vector_jet_eta[1]',
+                        'range' : (80,-5.0,5.0),
+                        'xaxis' : '#eta 2nd jet',
+                        'fold'  : 0
+                        }
+
