8e67d8b52cc2a60fdf1db72c85ef4ad5f4d9c3b4
Piergiulio <piergiulio.lenzi@cern.ch>
2019-01-09 11:06:32 +0100

    made inclusive control regions

--- Example1/cuts.py
+++ Example1/cuts.py
@@ -11,19 +11,19 @@ supercut = 'mll>12  \
 
 # cuts are defined with a name and the selection
 
-# DT->tautau control region with 0 jets
-cuts['hww2l2v_13TeV_dytt_of0j']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13)    \
+# DT->tautau control region
+cuts['hww2l2v_13TeV_dytt']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13)    \
                 && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
                 && ( mth<60) \
                 && mll>40 && mll<80 \
-                && ( std_vector_jet_pt[0] < 30 ) \
                 && '+bVeto+' \
                 '
-# top control region with 1 jet                
-cuts['hww2l2v_13TeV_top_of1j']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13)    \
+# top control region                
+cuts['hww2l2v_13TeV_top']  = '(std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13)    \
                 && (abs(std_vector_lepton_flavour[1]) == 13 || std_vector_lepton_pt[1]>13) \
                 && mll>50 \
                 && ( std_vector_jet_pt[0] >= 30 ) \
-                && ( std_vector_jet_pt[1] < 30 ) \
+                && ( std_vector_jet_pt[1] >= 30 ) \
                 && std_vector_jet_cmvav2[0]>-0.5884 \
+                && std_vector_jet_cmvav2[1]>-0.5884 \
                 '
